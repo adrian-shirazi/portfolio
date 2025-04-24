22f2f8c5d210662b4d522a5b2c168816e221c09f
ashirazi01 <adrian01@vt.edu>
2025-04-24 15:54:55 -0400

    Adds project cards to project section

--- projects.js
+++ projects.js
@@ -2,10 +2,20 @@ const response = await fetch("./projects.json");
 
 const json = await response.json();
 
-json.forEach((element) => {
-  const newDiv = document.createElement("div");
-  const newContent = document.createTextNode(element.name);
-  newDiv.appendChild(newContent);
-  const currentDiv = document.getElementById("projects-container");
-  currentDiv.insertBefore(newDiv, currentDiv);
+json.forEach((project) => {
+  const card = document.createElement("div");
+  card.className = "card";
+
+  card.innerHTML = `
+    <a href="${project.link}" target="_blank" rel="noopener noreferrer">
+      <img src="${project.thumbnail}" alt="${project.name}" class="card-image">
+    </a>
+    <div class="card-content">
+      <h3 class="card-title">${project.name}</h3>
+      <p class="card-description">${project.description}</p>
+      <a href="${project.github}" target="_blank" rel="noopener noreferrer" class="card-link">View Source Code</a>
+    </div>
+  `;
+  const projectsContainer = document.getElementById("projects-container");
+  projectsContainer.appendChild(card);
 });
